042e16167dd2161dd9fe42df3248b3c111cabda7
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-05-28 20:29:02 +0200

    Update App

--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # Ordenador de numeros
 
 Este **programa** usa
+
+
+**Problemas**
+
+1º Crear una cantidad de numeros **(array)**
+
+2º Ordenar numeros ascendente o descendente **(algoritmo de ordenación)**
+
+3º Evitar poner el **numero 4**
+
+4º Hacer programa **funcional**
+
--- src/main/java/ns/App.java
+++ src/main/java/ns/App.java
@@ -1,33 +1,90 @@
 package ns;
 
+
 import java.util.Scanner;
 
 public final class App {
     
+    
+    
     public static void main(String[] args) {
-        
+    
         Scanner input = new Scanner(System.in);
-        
-        
-        System.out.print("Please enter a number: ");
 
-        int number = 0;
 
-        while (!input.hasNextInt()) {
+        int[] array  = {6, 3, 1, 43, 9, 10, 45, 4, 8};
+        boolean condition = true;
+        int arrayCopy;
+
+
+        System.out.print("Ingresa 1 o 2 para ascendente o descendente: ");
+        int size = input.nextInt();
+
+      
+        while (true) {
             
-            System.out.println("It isn't a numeric value");
-            System.out.print("Please enter a number: ");
-            input.nextLine();
+        
+            condition=false;
+            for (int i=1; i < array.length; i++){
+           
+                if (array[i]<array[i-1]) {
+            
+                    arrayCopy=array[i];
+                    array[i]=array[i-1];
+                    array[i-1]=arrayCopy; 
+                    condition=true;
+                }
+
+
+            }
+            if (condition==false) {
+                break;
+            }
+
+
+       }
+
+     
+
+
+
+
+       if (size==1) {
+        
+        for (int iPrint=0; iPrint < array.length; iPrint++){
+
+            System.out.print(array[iPrint] + "  ");
+
         }
 
-        number = input.nextInt();
+
+       }else{
+
+
+        for (int iPrint=array.length-1; iPrint >= 0 ; iPrint--){
+
+            System.out.print(array[iPrint] + "  ");
+        
+        }
+
+
+       }
 
         
-        System.out.println("The number is: "+number);
+
+    
 
 
+        
+        
+
+
+
+      
+
+        
 
         input.close();
-
+        
     }
 }
